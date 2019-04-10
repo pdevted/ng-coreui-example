67f95f2ccbb8f2b12788498f27d8f682a0b4f5fc
Ted Kim <yenafirst91@gmail.com>
2019-04-09 20:00:18 -0400

    Initial Commit

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { IProduct } from './product';
+import { Route } from '@angular/compiler/src/core';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id'); // + shortcut string
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2016",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+
+}
